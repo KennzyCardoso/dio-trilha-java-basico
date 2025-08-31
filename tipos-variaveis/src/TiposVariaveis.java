062f95df4b12bbd1b11634009b11961ff269b560
KennzyCardoso <kennzy418@gmail.com>
2025-08-31 09:48:15 -0300

    aprendendo sobre variavies

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
-public class App {
+public class TiposVariaveis{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //classe String representa texto na aplicação
+
+        String meuNome = "Kennzy";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal =  numeroCurto;
+        short numertoCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
