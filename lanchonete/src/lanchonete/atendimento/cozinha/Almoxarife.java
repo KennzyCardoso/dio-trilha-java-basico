a02897459c62bf5e973c8920954214decb563867
KennzyCardoso <kennzy418@gmail.com>
2025-11-03 21:07:10 -0300

    Lanchonete

---
+++ lanchonete/src/lanchonete/atendimento/cozinha/Almoxarife.java
@@ -0,0 +1,20 @@
+package lanchonete.atendimento.cozinha;
+
+public class Almoxarife {
+    private void controlarEntrada() {
+        System.out.println("CONTROLANDO A ENTRADA DOS ITENS");
+    }
+
+    private void controlaSaida() {
+        System.out.println("CONTROLANDO A SAIDA DOS ITENS");
+    }
+
+    void entregarIngredientes() {
+        System.out.println("ENTREGANDO INGREDIENTES");
+        controlaSaida();
+    }
+
+    void trocarGas() {
+        System.out.println("ALMOXARIFE TROCANDO O GAS");
+    }
+}
